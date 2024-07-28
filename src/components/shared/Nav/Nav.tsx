2f67260e1a970332e08d9684abf7475453720891
Kevin Riquelme <kriquelme10@gmail.com>
2024-07-27 22:52:21 -0300

    working in navbar

--- src/components/shared/Nav/Nav.tsx
+++ src/components/shared/Nav/Nav.tsx
@@ -2,12 +2,21 @@ import Image from "next/image"
 import styles from './Nav.module.sass'
 
 
+import hamb from '/public/icons/menu-hamburger-svgrepo-com.svg'
+
+
 export const Nav = () => {
     return (
         <>              
-            <nav className={` ${styles.nav} flex flex-row items-center justify-between decoration-slate-800 `}>
-                <p>Logo</p>
-                <ul>
+            <nav className={` ${styles.nav} sm:w-full w-full h-[60px]  flex flex-row items-center justify-between decoration-slate-800 `}>
+                <Image 
+                    className="pl-4"
+                    src={hamb}
+                    width={45}
+                    height={45}
+                    alt="hamburger-menu"
+                />
+                <ul className="">
                     <li>Home</li>
                     <li>Acerca de</li>
                     <li>Servicios </li>
