89b97c8206a0e29ad4fc1f4f476281d3f9985f9f
Kevin Riquelme <kriquelme10@gmail.com>
2024-07-31 23:51:07 -0300

    carrusel optimizado con etiqueta Image

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,44 +1,76 @@
-'use client'
+"use client";
 
-import Carousel from 'react-bootstrap/Carousel'
-
+import Carousel from "react-bootstrap/Carousel";
+import Image from "next/image";
+import styles from './Hero.module.sass'
 
 export const Hero = () => {
-      return (
-            <>
-                  <Carousel className='bg-gray-400'>
-                        <Carousel.Item>
-                        <div className='w-full h-40 flex justify-center items-center'>
-                              <h1>Primer Slide</h1>
-                        </div>
-                        <Carousel.Caption>
-                        {/* <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
-                        </Carousel.Caption>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                        {/* <ExampleCarouselImage text="Second slide" /> */}
-                        <div className='w-full h-40 flex justify-center items-center'>
-                              <h1>Segundo Slide</h1>
-                        </div>
-                        <Carousel.Caption>
-                        {/* <h3>Second slide label</h3>
+  return (
+    <>
+      <Carousel data-bs-theme="dark" className={`${styles['main-container']}`} >
+        <Carousel.Item>
+          <div className="relative w-full h-96">
+            <Image
+              src="/images-carrusel/image1.jpg"
+
+              //llenar
+              fill={true}
+
+              //Largest Contentful Paint (LCP) es una métrica que mide
+              // el tiempo que tarda en cargarse y renderizarse
+              //priority
+
+              // El atributo sizes es importante para que el navegador sepa
+              //  qué tamaño de imagen debe descargar en función del tamaño de 
+              //  la ventana del navegador mejorando así el rendimiento de la página
+              sizes="(max-width: 768px) 100vw, 33vw"
+              alt="image-carrusel"
+              
+              //Fit:adaptar
+              //atributo actualizado
+              style={{ objectFit: 'contain' }}
+            />
+            <h1>Primer Slide</h1>
+          </div>
+          <Carousel.Caption></Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          {/* <ExampleCarouselImage text="Second slide" /> */}
+          <div className="relative w-full h-96">
+            <Image
+              src="/images-carrusel/image2.jpg"
+              alt="image-carrusel"
+              sizes="(max-width: 768px) 100vw, 33vw"
+              fill={true}
+              style={{ objectFit: 'contain' }}
+            />
+            <h1>Segundp Slide</h1>
+          </div>
+          <Carousel.Caption>
+            {/* <h3>Second slide label</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
-                        </Carousel.Caption>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                        {/* <ExampleCarouselImage text="Third slide" /> */}
-                        <div className='w-full h-40  flex justify-center items-center'>
-                              <h1>Tercero Slide</h1>
-                        </div>
-                        <Carousel.Caption>
-                        {/* <h3>Third slide label</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          {/* <ExampleCarouselImage text="Third slide" /> */}
+          <div className="relative w-full h-96">
+            <Image
+              src="/images-carrusel/image3.jpg"
+              alt="image-carrusel"
+              fill={true}
+              sizes="(max-width: 768px) 100vw, 33vw"
+              style={{ objectFit: 'contain' }}
+            />
+            <h1>Tercer Slide</h1>
+          </div>
+          <Carousel.Caption>
+            {/* <h3>Third slide label</h3>
                         <p>
                               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
                         </p> */}
-                        </Carousel.Caption>
-                        </Carousel.Item>
-                  </Carousel>
-            </>
-      )
-}
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </>
+  );
+};
