d8b6ec614f86d602e4e1389e36fc2ebc57b829cd
Kevin Riquelme <kriquelme10@gmail.com>
2024-08-02 00:39:00 -0300

    carpetas main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import { Hero } from "@/components/Hero"
+import { Main } from "@/components/Main"
 
 export default function Home () {
   return (
-    <Hero  />
+    <>
+      <Hero  />
+      <Main />
+    </>
   )
 }
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Main = () => {
+      return (
+            <div className='flex w-full h-auto justify-center items-center' >
+                <h1 className=''>Muebles de diseño</h1>  
+            </div>
+      );
+}
+
+
