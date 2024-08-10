284bb11e05c290b53fbdd8d0a7bc80a81f7fab33
Kevin Riquelme <kriquelme10@gmail.com>
2024-08-10 13:20:32 -0300

    responsive footer

--- src/components/shared/Footer/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer/Footer.tsx
@@ -10,9 +10,9 @@ export const Footer = () => {
       <footer className={`text-white py-4 mt-auto ${styles['container-footer']}`}>
         <div className="container mt-12 sm:px-8 ">
           <div className="flex flex-col gap-3 sm:flex-row sm:justify-between lg:mx-40">
-            <div className="flex flex-col justify-start items-center">
-              <h5 className="text-xl">Visita nuestras redes</h5>
-              <ul className="flex flex-col gap-3 mt-12 ml-4">
+            <div className="flex flex-col justify-start items-center mb-3 md:!mb-0">
+              <h5 className="text-xl border-b-[1px] pb-1">Visita nuestras redes</h5>
+              <ul className="flex flex-col gap-3 mt-6 ml-4 font-light">
                 <li className="flex gap-4 ">
                   <Image 
                         width={30}
@@ -49,34 +49,52 @@ export const Footer = () => {
               </ul>
             </div>
             <div className="col-md-4 flex flex-col items-center justify-center">
-              <h5 className="text-xl text-center">Contacto</h5>
-              <address className="flex flex-col gap-3 mt-12 ml-4 text-center">
-                <div className="flex">
-                  <Image 
-                    width={30}
-                    height={30}
-                    src={'/googlemaps.svg'}
-                    alt="whatsapp"
-                    className="inline mr-6"
-                  />
-                  <div className="inline-block text-start">
-                   Calle Falsa 123
-                    <br />
-                    General Roca, Río Negro
-                    <br />
+              <h5 className="text-xl text-center  border-b-[1px] pb-1">Contacto</h5>
+              <address className="flex flex-col gap-3 mt-6 ml-4 text-center">
+                <div className="flex flex-col gap-4 text-center font-light">
+                  <div className="flex items-center gap-2">
+                    <Image 
+                      width={30}
+                      height={30}
+                      src={'/googlemaps.svg'}
+                      alt="location"
+                      className=""
+                    />
+                    <div className="inline-block text-start whitespace-nowrap">
+                      Av. Roca 123
+                      <br />
+                      General Roca, Río Negro
+                    </div>
+                  </div>
+                  <div className="flex items-center gap-2">  
+                    <Image 
+                        width={30}
+                        height={30}
+                        src={'/mailfree.svg'}
+                        alt="mail"
+                        className=""
+                      />
+                    <a href="#" className="text-white">
+                      email@ejemplo.com
+                    </a>
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <Image 
+                          width={30}
+                          height={30}
+                          src={'/phonefree.svg'}
+                          alt="phone"
+                          className=""
+                        />
+                    <span>Cel: +123 456 7890</span>
                   </div>
                 </div>
              
-                <a href="mailto:email@ejemplo.com" className="text-white">
-                  email@ejemplo.com
-                </a>
-                <br />
-                Teléfono: +123 456 7890
               </address>
             </div>
           </div>
           <div className="row mt-16">
-            <div className="col text-center">
+            <div className="col text-center font-light">
               <p className="mb-0">
                 &copy; 2024 MUEBLES PATAGONIA. Todos los derechos reservados.
               </p>
