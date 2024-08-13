07c85b932ab66c02988578fd3f6ed23172bc12a6
Kevin Riquelme <kriquelme10@gmail.com>
2024-08-13 01:40:50 -0300

    flag

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -11,10 +11,12 @@ const abrilFatface = Abril_Fatface ({
 export const AboutUs = () => {
       return (
             <div className={` ${styles['container-main']} flex flex-col mt-10 justify-center items-center text-center px-5 md:gap-11 mb-8 md:mb-16` }  id='about-us'>
-                  <div className='mb-14 flex flex-col mt-9 gap-4  lg:flex-row '>
-                        <h1 className={`${styles['title-section']}  text-center uppercase text-5xl max-w-max  font-semibold md:text-6xl md:whitespace-nowrap`}>Sobre nosotros</h1>
-                        <Button variant="dark" className='self-center w-44 md:w-48 md:h-11 md:ml-3 uppercase' >contactanos</Button>{' '}
-                  </div>
+                  <a href="#form">
+                        <div className='mb-14 flex flex-col mt-9 gap-4  lg:flex-row ' >
+                              <h1 className={`${styles['title-section']}  text-center uppercase text-5xl max-w-max  font-semibold md:text-6xl md:whitespace-nowrap`}>Sobre nosotros</h1>
+                              <Button variant="dark" className='self-center w-44 md:w-48 md:h-11 md:ml-3 uppercase bg-yellow-400 hover:bg-yellow-500 border-3 text-black border-black' >contactanos</Button>{' '}
+                        </div>
+                  </a>
                   <div className='flex flex-col md:flex-row md:gap-5 ' > 
                         <h2 className={`${abrilFatface.className} self-center  text-2xl mb-2 md:w-1/4 md:text-2xl md:self-start`}>Nuestra historia</h2>  
                         <p className={` ${styles['p-afer']} relative mb-6 font-light md:w-3/4 md:p-6 md:text-center md:self-start md:py-0 md:px-20`}>Desde nuestros inicios, hemos creído en el poder del diseño para transformar espacios y vidas. Fundada en 2010, nuestra empresa nació del deseo de ofrecer muebles que combinan funcionalidad y estética. Comenzamos como un pequeño taller artesanal y, con el tiempo, hemos crecido hasta convertirnos en una marca reconocida por nuestra dedicación a la calidad y el diseño innovador. Cada pieza que ofrecemos es el resultado de una meticulosa atención al detalle y un profundo respeto por los materiales.</p>    
